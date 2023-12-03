1ed6544c81e282dd7b4cb84882d366097d7c3b0a
Minjalidze <73029287+Minjalidze@users.noreply.github.com>
2023-12-04 01:48:05 +0700

    working deeplinks

--- app/src/main/java/com/minjalidze/anonimousvotes/ui/visuals/screens/HomeScreen.kt
+++ app/src/main/java/com/minjalidze/anonimousvotes/ui/visuals/screens/HomeScreen.kt
@@ -2,10 +2,8 @@ package com.minjalidze.anonimousvotes.ui.visuals.screens
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentSize
@@ -15,46 +13,53 @@ import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.pullrefresh.PullRefreshIndicator
 import androidx.compose.material.pullrefresh.pullRefresh
 import androidx.compose.material.pullrefresh.rememberPullRefreshState
+import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ModalBottomSheet
+import androidx.compose.material3.Text
 import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Alignment.Companion.CenterHorizontally
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.asImageBitmap
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import com.minjalidze.anonimousvotes.data.api.API
 import com.minjalidze.anonimousvotes.data.api.API.Companion.getVotes
 import com.minjalidze.anonimousvotes.ui.theme.DDarkBlack
+import com.minjalidze.anonimousvotes.ui.theme.DGray
 import com.minjalidze.anonimousvotes.ui.visuals.elements.VoteModelCard
 import com.minjalidze.anonimousvotes.ui.visuals.elements.rememberQrBitmapPainter
-import kotlinx.coroutines.delay
+import kotlinx.coroutines.DelicateCoroutinesApi
+import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 
-@OptIn(ExperimentalMaterial3Api::class, ExperimentalMaterialApi::class)
+@OptIn(ExperimentalMaterial3Api::class, ExperimentalMaterialApi::class,
+    DelicateCoroutinesApi::class
+)
 @Composable
-fun HomeScreen() {
+fun HomeScreen(voteID: Int = -1, fromLink: Boolean = false) {
     val votes = getVotes()
 
-    val refreshScope = rememberCoroutineScope()
+    val sheetState = rememberModalBottomSheetState()
+
+    var isSheetOpen by rememberSaveable { mutableStateOf(false) }
+    var currentVoteID by remember { mutableIntStateOf(voteID) }
     var refreshing by remember { mutableStateOf(false) }
 
-    val sheetState = rememberModalBottomSheetState()
-    var isSheetOpen by rememberSaveable {
-        mutableStateOf(false)
-    }
+    var linkOpened by remember { mutableStateOf(false) }
 
-    fun refresh() = refreshScope.launch {
+    fun refresh() = GlobalScope.launch {
         refreshing = true
-        delay(1500)
         API.initialize()
         refreshing = false
     }
@@ -71,7 +76,10 @@ fun HomeScreen() {
             if (!refreshing) {
                 LazyColumn {
                     items(votes) {
-                        VoteModelCard(it) { isSheetOpen = true }
+                        VoteModelCard(it) {
+                            currentVoteID = it.id
+                            isSheetOpen = true
+                        }
                     }
                 }
             }
@@ -79,17 +87,30 @@ fun HomeScreen() {
         }
     }
 
+    if (fromLink && !linkOpened) {
+        currentVoteID = voteID
+        isSheetOpen = true
+        linkOpened = false
+    }
+
     if (isSheetOpen) {
-        ModalBottomSheet(onDismissRequest = { isSheetOpen = false }, sheetState = sheetState) {
-            Row (modifier = Modifier
-                .padding(8.dp).align(CenterHorizontally).background(Color.Transparent),
-                horizontalArrangement = Arrangement.Center,
-                verticalAlignment = Alignment.Top) {
+        val vote = API.selectVote(currentVoteID)
+        ModalBottomSheet(onDismissRequest = { isSheetOpen = false }, sheetState = sheetState, containerColor = DGray) {
+            Column (modifier = Modifier
+                .padding(8.dp)
+                .align(CenterHorizontally)) {
                 Image(
-                    painter = rememberQrBitmapPainter(content = "app://com.minjalidze.anonymousvotes", size = 250.dp, padding = 8.dp),
+                    bitmap = rememberQrBitmapPainter(content = "app://com.minjalidze.anonymousvotes/vote/$currentVoteID").asImageBitmap(),
                     contentDescription = "test qr code",
-                    contentScale = ContentScale.FillBounds
+                    contentScale = ContentScale.FillBounds,
+                    modifier = Modifier.background(Color.Gray).align(CenterHorizontally)
                 )
+                Divider(modifier = Modifier.padding(8.dp))
+                Text(text = vote!!.title,
+                    fontWeight = FontWeight.Bold,
+                    modifier = Modifier
+                        .padding(8.dp)
+                        .align(CenterHorizontally))
             }
         }
     }
